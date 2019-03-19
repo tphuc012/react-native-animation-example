7adfde38bc12e3aae591e3707bbdbb36b9b52cb7
phuc le <tphuc012@phucs-MacBook-Air.local>
2019-03-19 11:28:33 +0700

    add decay demo with panresponder

--- src/12-decay/index.js
+++ src/12-decay/index.js
@@ -1,14 +1,58 @@
 import React, { Component } from 'react';
-import { Text, StyleSheet, View } from 'react-native';
+import { Text, StyleSheet, View, PanResponder, Animated } from 'react-native';
 
 export default class DecayScreen extends Component {
+  state = { animation: new Animated.ValueXY({ x: 0, y: 0 }) };
+  UNSAFE_componentWillMount() {
+    this._x = 0;
+    this._y = 0;
+    this.state.animation.addListener(value => {
+      this._x = value.x;
+      this._y = value.y;
+    });
+    this._panResponder = PanResponder.create({
+      onStartShouldSetPanResponder: () => true,
+      onMoveShouldSetPanResponder: () => true,
+      onPanResponderGrant: (e, gestureState) => {
+        //   this.state.animation.setOffset({
+        //     x: this._x,
+        //     y: this._y
+        //   });
+        //   this.state.animation.setValue({ x: 0, y: 0 });
+        //OR function below
+        this.state.animation.extractOffset();
+      },
+
+      onPanResponderMove: Animated.event([
+        null,
+        {
+          dx: this.state.animation.x,
+          dy: this.state.animation.y
+        }
+      ]),
+      onPanResponderRelease: (e, { vx, vy }) => {
+        Animated.decay(this.state.animation, {
+          velocity: { x: vx, y: vy },
+          deceleration: 0.97
+        }).start();
+      }
+    });
+  }
   render() {
+    const animatedStyle = {
+      transform: this.state.animation.getTranslateTransform()
+    };
     return (
       <View>
-        <Text> textInComponent </Text>
+        <Animated.View
+          style={[styles.box, animatedStyle]}
+          {...this._panResponder.panHandlers}
+        />
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  box: { width: 100, height: 100, backgroundColor: 'red' }
+});
